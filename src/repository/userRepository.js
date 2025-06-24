120361400d6fede7071b04b97b2a78e3b24f4381
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-24 18:11:32 +0530

    Feat: Added a Crud Repository and User Repository

---
+++ src/repository/userRepository.js
@@ -0,0 +1,23 @@
+import User from "../schema/userSchema.js";
+import crudRepository from "./crudRepository.js";
+
+const userRepository = {...crudRepository(User),
+
+    getUserByEmail: async(email) =>{
+    const user = await User.findOne({email});
+    return user
+    },
+
+    getUserByName: async(name) =>{
+    const user = await User.findOne({username: name}).select('-password'); // exclude password
+    return user;
+    }
+}
+
+
+export default new userRepository();
+
+
+
+
+
