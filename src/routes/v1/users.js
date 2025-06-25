950f64a1dcda34c1267e9b1f5705113b2dc8789e
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-25 07:16:09 +0530

    Feat: Add validation in the user route

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { signup } from '../../controllers/userController.js';
+import { userSignUpSchema } from '../../validators/userSchema.js';
+import { validate } from '../../validators/zodValidate.js';
 
 const router = express.Router();
 
 
  
-router.post('/signup', signup);
+router.post('/signup', validate(userSignUpSchema), signup);
 
 
 export default router;
